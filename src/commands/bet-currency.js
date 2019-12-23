5efad6c076d3598c67bbad44a7709af0b12d0622
Tim Ziegel <tziegel@thrillworks.com>
2019-12-23 17:35:27 -0500

    Add eligibility period to currency bets

--- src/commands/bet-currency.js
+++ src/commands/bet-currency.js
@@ -1,14 +1,22 @@
 import madlibs from 'mad-libber';
+import dayjs from 'dayjs';
+import RelativeTime from 'dayjs/plugin/relativeTime';
 import { Command } from '../lib/command';
 import { currency, defaultCurrency } from '../lib/currency';
 import { getRandom, getRandomNumberBetween, humanize } from '../lib/utils';
 import { colors } from '../lib/colors';
+import { users } from '../lib/users';
+import db, { timestamp } from '../lib/database';
+
+dayjs.extend(RelativeTime);
 
 export class BetCurrencyCommand extends Command {
   command = 'bet';
   aliases = ['roll'];
-  help = 'Bet some gold and roll the die! Rolling 50+ yields 1.5x gold. Rolling 75+ yields 2x gold. Rolling 90+ yields 4x. Rolling 100 yields 10x.';
+  help = 'Bet some gold and roll the die! Rolling 50+ yields 1.5x gold. Rolling 75+ yields 2x gold. Rolling 90+ yields 3x. Rolling 100 yields 5x.';
   example = '!bet 10';
+  eligibilityPeriod = 86400000; // One day in ms
+  betsPerPeriod = 15; // Max bids per period
   
   data = {
     unlucky: [
@@ -69,12 +77,25 @@ export class BetCurrencyCommand extends Command {
   
   async run(message) {
     // TODO: infer type from message
+    const user = message.author;
+    const betsThisPeriod = await this.getBetsThisPeriod(user);
+    if (betsThisPeriod.size > this.betsPerPeriod) {
+      const hours = Math.ceil(this.eligibilityPeriod / 3600000);
+      const now = timestamp();
+      const earliestBet = betsThisPeriod[0].data().timestamp;
+      const timeUntilNext = now - earliestBet + this.eligibilityPeriod;
+      const nextAvailableMessage = dayjs(now).to(dayjs(timeUntilNext));
+      return this.post(`You've reached your limit for today! You can only bet ${this.betsPerPeriod} times every ${hours} hours. Your next bet will be available ${nextAvailableMessage}.`);
+    }
+    
     const { type, color } = defaultCurrency;
     const amountString = message.content.match(/\s\d+\b/i);
     if (!amountString) return this.post(`Whoops! You must specify an amount to bet.`, message);
+    
     const amount = parseInt(amountString[0], 10);
     if (!amount) return this.post(`Whoops! You can't bet that much.`, message);
-    const currentAmount = await currency.amount(message.author, type);
+    
+    const currentAmount = await currency.amount(user, type);
     if (amount > currentAmount) return this.post(`Whoops! You can't bet ${humanize(amount)} ${type}. You've only got ${humanize(currentAmount)}.`, message);
     
     const result = this.roll(amount, type);
@@ -88,8 +109,9 @@ export class BetCurrencyCommand extends Command {
       }
     };
     
-    const promise = result.won ? currency.give(message.author, result.winnings - amount) : currency.take(message.author, amount);
+    const promise = result.won ? currency.give(user, result.winnings - amount) : currency.take(user, amount);
     return promise
+      .then(() => this.logBet(user, result))
       .then(() => this.postEmbed(embedOptions, message))
       .catch(e => this.post(`Whoops! Something went wrong with your bet. Sorry, eh.`, message));
   }
@@ -104,11 +126,11 @@ export class BetCurrencyCommand extends Command {
     const replacements = { amount: [humanize(amount)], roll: [roll + ''], type: [type + ''] };
     
     if (roll === 100) {
-      multiplier = 10;
+      multiplier = 5;
       winnings = Math.floor(amount * multiplier);
       title = madlibs(getRandom(this.data.insane), replacements);
     } else if (roll >= 90) {
-      multiplier = 4;
+      multiplier = 3;
       winnings = Math.floor(amount * multiplier);
       title = madlibs(getRandom(this.data.crazy), replacements);
     } else if (roll >= 75) {
@@ -130,6 +152,7 @@ export class BetCurrencyCommand extends Command {
     else message = `You lost ${humanize(amount)} ${type}.`;
     
     return {
+      amount,
       won,
       roll,
       multiplier,
@@ -137,6 +160,23 @@ export class BetCurrencyCommand extends Command {
       title,
       message
     };
+  }
+  
+  async getBetsThisPeriod(user) {
+    const userId = users.getId(user);
+    const periodStart = timestamp() - this.eligibilityPeriod;
+    const query = db.pipe(
+      db.table('bets'),
+      db.where$('userId', '==', userId),
+      db.orderBy$('timestamp'),
+      db.startAt$(periodStart)
+    );
+    return query.get();
+  }
+  
+  async logBet(user, roll) {
+    const userId = users.getId(user);
+    return db.add('bets', { userId, roll });
   }
 }
 
--- src/lib/database.js
+++ src/lib/database.js
@@ -1,6 +1,11 @@
-import Firestore from '@google-cloud/firestore';
-import { FieldValue, CollectionReference, Query } from '@google-cloud/firestore';
-import { isTestBot } from './utils';
+import Firestore from "@google-cloud/firestore";
+import {
+  FieldValue,
+  CollectionReference,
+  Query,
+  Timestamp
+} from "@google-cloud/firestore";
+import { isTestBot } from "./utils";
 
 const { PROJECT_ID, GOOGLE_CREDENTIALS } = process.env;
 
@@ -13,42 +18,93 @@ if (isTestBot()) {
 
 const database = new Firestore(options);
 export const increment = amount => FieldValue.increment(amount);
-export const timestamp = () => FieldValue.serverTimestamp();
+export const timestamp = date => {
+  const stamp = date ? Timestamp.fromDate(date) : Timestamp.now();
+  return stamp.toMillis();
+};
 
 export class Database {
-  
   async get(id, table) {
-    const doc = await this.table(table).doc(id).get();
+    const doc = await this.table(table)
+      .doc(id)
+      .get();
     return doc.exists ? doc.data() : null;
   }
-  
+
   async set(id, table, data = {}, merge = true) {
     const newData = { ...data, timestamp: timestamp() };
     const doc = await this.table(table).doc(id);
     return doc.set(newData, { merge });
   }
-  
+
+  async add(table, data) {
+    const newData = { ...data, timestamp: timestamp() };
+    return this.table(table).add(newData);
+  }
+
   async getAll(table) {
     const { docs } = await this.table(table).get();
     return docs.map(doc => ({ id: doc.id, ...doc.data() }));
   }
-  
+
   table(id) {
     if (id instanceof CollectionReference || id instanceof Query) return id;
     if (isTestBot()) id = `staging-${id}`;
     return database.collection(id);
   }
-  
-  orderBy(table, order, direction = 'asc') {
+
+  orderBy(table, order, direction = "asc") {
     return this.table(table).orderBy(order, direction);
   }
-  
+
+  orderBy$(order, direction = "asc") {
+    return query => query.orderBy(order, direction);
+  }
+
+  startAt(table, value) {
+    return this.table(table).startAt(value);
+  }
+
+  startAt$(value) {
+    return query => query.startAt(value);
+  }
+
   where(table, field, comparator, value) {
     return this.table(table).where(field, comparator, value);
   }
-  
+
+  where$(field, comparator, value) {
+    return query => query.where(field, comparator, value);
+  }
+
+  whereAll(table, ...comparators) {
+    const query = this.table(table);
+    return comparators.reduce(
+      (acc, [field, comparator, value]) =>
+        this.whereChain(acc, field, comparator, value),
+      query
+    );
+  }
+
+  whereAll$(...comparators) {
+    return query =>
+      comparators.reduce(
+        (acc, [field, comparator, value]) =>
+          this.whereChain(acc, field, comparator, value),
+        query
+      );
+  }
+
   select(table, ...fields) {
     return this.table(table).select(...fields);
+  }
+
+  select$(...fields) {
+    return query => query.select(...fields);
+  }
+
+  pipe(query, ...chains) {
+    return chains.reduce((acc, curr) => curr(acc), query);
   }
 }
 
